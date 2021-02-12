0936b12e136c964a81ffeda7418003fb049dd569
Andra Antariksa <andra.antariksa@gmail.com>
2021-02-12 22:37:22 +0700

    Add grid

---
+++ Assets/Game/Pathing/Node.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    ArrayList<Node> parent;
+    ArrayList<Node> child;
+    Vector2 pos;
+
+    void Start()
+    {
+        
+    }
+}
---
+++ Assets/Game/Pathing/Pathing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathing : MonoBehaviour
+{
+    ArrayList<Vector2> starting;
+
+    void Start()
+    {
+        
+    }
+
+    int EuclideanDistance(Vector2 a, Vector2 b)
+    {
+        return (int) Mathf.Sqrt(Mathf.Pow(node.x - goal.x, 2) +
+            Mathf.Pow(node.y - goal.y, 2) +
+            Mathf.Pow(node.z - goal.z, 2));
+    }
+
+    void GetShortestPathAStar(Node target)
+    { 
+        for (Node source: starting)
+        {
+
+        }
+    }
+
+    bool IsRootConnectedTo(Node target)
+    {
+        Priority
+        return false;
+    }
+}
