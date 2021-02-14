ce2ecdafc1d5aa88a26612e6ebd0700ea0607397
Andra Antariksa <andra.antariksa@gmail.com>
2021-02-14 18:39:08 +0700

    Implement game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    GameObject thingsAboveGroundObj;
+    List<Building> buildings = new List<Building>();
+
     void Start()
+    {
+        PopulateBuildings();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void PopulateBuildings()
     {
-        
+        var buildingComponents = thingsAboveGroundObj.GetComponentsInChildren<Building>();
+        foreach (var building in buildingComponents)
+        {
+            if (building != null)
+            {
+                buildings.Add(building);
+            }
+        }
+    }
+
+    float TotalScore()
+    {
+        float moodTotal = 0;
+        foreach (var building in buildings)
+        {
+            moodTotal += building.moodAmount;
+        }
+
+        return moodTotal;
     }
 }
