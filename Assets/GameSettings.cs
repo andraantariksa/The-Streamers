a001c25064881f710e26100ec340214996119a1b
Andra Antariksa <andra.antariksa@gmail.com>
2021-02-14 18:13:17 +0700

    Add audio support

---
+++ Assets/GameSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings
+{
+    static private GameSettings instance = new GameSettings();
+
+    static public GameSettings Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public float volume = 1.0f;
+    public bool isSoundOn = true;
+}
